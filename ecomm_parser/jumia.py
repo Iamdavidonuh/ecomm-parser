a58856e8d7ce4f91b8edd542bb96c4e12bf99a18
David Onuh <youngstizy@gmail.com>
2019-09-30 16:02:11 +0100

    functionbased scrapper

--- ecomm_parser/jumia.py
+++ ecomm_parser/jumia.py
@@ -1,29 +1,38 @@
 from bs4 import BeautifulSoup as soup
 import requests
 
-page_raw = requests.get('https://www.jumia.com.ng/keyboards-mice-accessories/')
-html_file = page_raw.text
+def get_data(requests,url, soup):
+    """ queries the site, basic processing and returns the result_set we need"""
+    page_raw = requests.get(url)
+    html_file = page_raw.text
+    raw_data = soup(html_file, "lxml")
+    result_set = raw_data.find_all(True, {'class':['sku -gallery', '-has-offers']}) 
+    result = result_set[0]
+    return result
 
-raw_data = soup(html_file, "lxml")
+def parse_data(result):
+    #getting important stuffs needed
+    _ = result.find(class_='title')
+    brand = _.find(class_='brand').text
+    title = _.find(class_='name').text
+    link = result.a['href']
+    img = result.img['data-src']
+    #currency = result.find(class_="price").span['data-currency-iso']
+    old_price = result.find(True, {'class': '-old'}).text
+    price = result.find(class_="price").text
+    discount = result.find(class_="sale-flag-percent").text
 
-result_set = raw_data.find_all(True, {'class':['sku -gallery', '-has-offers']}) 
-result = result_set[0]
+    #printing results
+    print(brand)
+    print(title)
+    print(link)
+    print(old_price)
+    print(price)
+    print(discount)
 
-#getting important stuffs needed
-_ = result.find(class_='title')
-brand = _.find(class_='brand').text
-title = _.find(class_='name').text
-link = result.a['href']
-img = result.img['data-src']
-#currency = result.find(class_="price").span['data-currency-iso']
-old_price = result.find(True, {'class': '-old'}).text
-price = result.find(class_="price").text
-discount = result.find(class_="sale-flag-percent").text
+def run():
+    result = get_data(requests,'https://www.jumia.com.ng/keyboards-mice-accessories/',soup)
+    parse_data(result)
 
-#printing results
-print(brand)
-print(title)
-print(link)
-print(old_price)
-print(price)
-print(discount)
+if __name__ == '__main__':
+    run()
